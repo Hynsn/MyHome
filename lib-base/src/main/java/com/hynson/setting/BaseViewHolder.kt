b73b6cccc80e4861e72737be83ef2765c57b96f1
Hynsn <hynson@163.com>
2022-06-03 20:52:24 +0800

    删除设置页代码

--- lib-base/src/main/java/com/hynson/setting/BaseViewHolder.kt
+++
@@ -1,9 +0,0 @@
-package com.hynson.setting
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-abstract class BaseViewHolder<T>(itemView: View) : RecyclerView.ViewHolder(itemView){
-    abstract val bean:T
-    abstract fun bindView():BaseViewHolder<T>
-}
--- lib-base/src/main/java/com/hynson/setting/ItemManager.kt
+++
@@ -1,9 +0,0 @@
-package com.hynson.setting
-
-import android.util.SparseArray
-import android.util.SparseIntArray
-
-object ItemManager {
-    val list = SparseIntArray()
-    val listViewHolder = SparseArray<String>()
-}
